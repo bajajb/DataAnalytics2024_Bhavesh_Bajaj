8613ea5d71beeaf6731a533f1285a3ddc9aea415
bhavesh42 <bajajb@rpi.edu>
2024-09-13 14:41:13 -0400

    Lab 1

---
+++ Lab1/Lab1.R
@@ -0,0 +1,42 @@
+# exercise 1
+EPI_data <- read.csv("C:/Users/bajajb/Documents/DataAnalytics/epi2024results06022024.txt")
+View(EPI_data)
+attach(EPI_data)
+EPI.new
+tf <- is.na(EPI.new)
+E <- EPI.new[!tf]
+summary(EPI.new)
+fivenum(EPI.new, na.rm=TRUE)
+stem(EPI.new)
+boxplot(EPI.new,APO.new)
+hist(EPI.new)
+hist(EPI.new, seq(20.,80.,1.0),prob=TRUE)
+lines(density(EPI.new,na.rm=TRUE,bw=1.))
+rug(EPI.new)
+hist(EPI.new, seq(20.,80.,1.0),prob=TRUE)
+lines(density(EPI.new,na.rm=TRUE,bw="SJ"))
+rug(EPI.new)
+x<-seq(20,80,1)
+q<-dnorm(x,mean=42,sd=5,log=FALSE)
+lines(x,q)
+lines(x,.4*q)
+q<-dnorm(x,mean=65,sd=5,log=FALSE)
+lines(x,.12*q)
+
+# exercise 2
+plot(ecdf(EPI.new),do.points=FALSE,verticals=TRUE)
+qqnorm(EPI.new);qqline(EPI.new)
+qqplot(rnorm(250),EPI.new,xlab="Q-Q plot for norm dsn")
+qqline(EPI.new)
+qqplot(rt(250,df=5),EPI.new,xlab="Q-Q plot for t dsn")
+qqline(EPI.new)
+
+# exercise 2a
+qqplot(rt(250,df=5),APO.new,xlab="Q-Q plot for norm dsn - APO")
+qqline(APO.new)
+qqplot(rt(250,df=5),APO.new,xlab="Q-Q plot for t dsn - APO")
+qqline(APO.new)
+qqplot(rt(250,df=5),WRS.new,xlab="Q-Q plot for norm dsn - WRS")
+qqline(WRS.new)
+qqplot(rt(250,df=5),WRS.new,xlab="Q-Q plot for t dsn - WRS")
+qqline(WRS.new)
